e6f40c2efd7f1dac8e75eaed15197073847c8d06
Pratik Ramteke <pratikramteke222@gmail.com>
2023-02-25 14:22:10 +0530

    todo app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import style from "./style.css";
 export default function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
@@ -12,8 +13,15 @@ export default function App() {
     todoNameRef.current.value = null;
   };
 
+  const editTodo = (id) => {
+    const updateTodo = todos.filter((elem, ind) => {
+      return ind === id;
+    });
+    todoNameRef.current.value = updateTodo;
+    deleteTodo(id);
+  };
+
   const deleteTodo = (id) => {
-    console.log(id);
     const updateTodo = todos.filter((elem, ind) => {
       return ind !== id;
     });
@@ -21,15 +29,25 @@ export default function App() {
   };
 
   return (
-    <div>
+    <div className="container">
       <input type="text" ref={todoNameRef} placeholder="Enter Todo" />
       <button onClick={createTodo}>Add</button>
-      <div>
+      <div className="todo-container">
         {todos.map((elem, ind) => {
           return (
-            <li key={ind} onClick={() => deleteTodo(ind)}>
-              {elem}
-            </li>
+            <div className="todo">
+              <span className={style} key={ind}>
+                {elem}
+              </span>
+              <div className="btn">
+                <button className="edit" onClick={() => editTodo(ind)}>
+                  Edit
+                </button>
+                <button className="delete" onClick={() => deleteTodo(ind)}>
+                  Delete
+                </button>
+              </div>
+            </div>
           );
         })}
       </div>
